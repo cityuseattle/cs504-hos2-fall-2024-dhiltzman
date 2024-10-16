be0f69606f8d20f20f904d9bc824fd15731219af
dhiltzman <david.hiltzman@gmail.com>
2024-10-16 13:36:01 -0700

    Submission for Module 02

---
+++ add_numbers.py
@@ -0,0 +1,11 @@
+"""
+This module contains a function to add two numbers.
+"""
+
+def add_two_numbers(x, y):
+    """
+    Adds two numbers and returns the result.
+    """
+    return x + y
+
+print(add_two_numbers(2,2))
